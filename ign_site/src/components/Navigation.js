30329d3f9197935281e03b9e89565726db810006
BluR3Y <69604218+BluR3Y@users.noreply.github.com>
2022-11-21 23:02:53 -0500

    Revert "Break points"

--- ign_site/src/components/Navigation.js
+++ ign_site/src/components/Navigation.js
@@ -7,15 +7,16 @@ import {
     ContentSelection, 
     SelectionList,
     SelectionList_More,
+    SearchBox,
     Profile,
     ThemeSelection,
-    StyledLoadingAdditionalContent,
+    AutoCompleteItem,
 } from "./styles/Navigation.styled";
 
 import Caret_Down from '../assets/icons/caret_down';
+import Magnifying_Glass from '../assets/icons/magnifying_glass';
 import Sun from '../assets/icons/sun';
 import Moon from '../assets/icons/moon';
-import Menu_Bars from '../assets/icons/menu_bars';
 
 import ProfileImg from '../assets/images/profileImg.jfif';
 import SearchBar from "./SearchBar";
@@ -24,35 +25,67 @@ export default class Navigation extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            hiddenContentItems: [],
-            additionalSelectionItems: null,
+            selectionItems: [
+                { id: 1, title: 'News', link: 'https://www.ign.com/news' },
+                { id: 2, title: 'Videos', link: 'https://www.ign.com/videos' },
+                { id: 3, title: 'Reviews', link: 'https://www.ign.com/reviews' },
+                { id: 4, title: 'Shows', link: 'https://www.ign.com/watch' },
+                { id: 5, title: 'Wikis', link: 'https://www.ign.com/wikis' },
+                { id: 6, title: 'IGN on social', link: 'https://www.ign.com/wikis/ign-community-central/How_to_Follow_IGN' },
+                { id: 7, title: 'About Us', link: 'https://corp.ign.com/' },
+                { id: 8, title: 'Accessibility', link: 'https://www.ziffdavis.com/accessibility' },
+                { id: 9, title: 'Privacy Policy', link: 'https://corp.ign.com/privacy' },
+                { id: 10, title: 'Terms of Use', link: 'https://corp.ign.com/user-agreement' },
+                { id: 11, title: 'Editorial Standards', link: 'https://corp.ign.com/standards-and-practices' },
+                { id: 12, title: 'Do Not Sell My Pesonal Information', link: 'https://corp.ign.com/ccpa' },
+                { id: 13, title: 'Site Map', link: 'https://corp.ign.com/sitemap' },
+                { id: 14, title: 'Boards', link: 'https://www.ignboards.com/' },
+                { id: 15, title: 'Contact Support', link: 'https://corp.ign.com/support' }
+            ],
+            visibleItems: [],
+            hiddenItems: [],
+            additionalSelectionItems: [
+                { id: 1, title: "Overwatch 2", link: 'https://www.ign.com/games/overwatch-2'},
+                { id: 2, title: "Gotham Knights", link: 'https://www.ign.com/games/gotham-knights'},
+                { id: 3, title: "Modern Warfare 2", link: 'https://www.ign.com/games/call-of-duty-modern-warfare-2'},
+                { id: 4, title: "Kingdom Hearts III", link: 'https://www.ign.com/games/kingdom-hearts-iii'},
+                { id: 5, title: "The Walking Dead", link: 'https://www.ign.com/games/the-walking-dead-the-game'},
+                { id: 6, title: "God of War", link: 'https://www.ign.com/games/god-of-war'},
+                { id: 7, title: "Marvel's The Avengers: Infinity War", link: 'https://www.ign.com/articles/2018/05/18/avengers-infinity-war-review'},
+                { id: 8, title: "Super Troopers 2", link: 'https://www.ign.com/movies/super-troopers-2'},
+                { id: 9, title: "Marvel's The Defenders", link: 'https://www.ign.com/articles/2017/08/18/marvels-the-defenders-season-1-review'},
+            ],
             selectionListRef: React.createRef(),
-            searchBarRef: React.createRef(),
-            currentDate: null,
+            searchBarRef: React.createRef()
         }
     }
 
     componentDidMount() {
-        this.getDate();
-        this.getTrendingGames();
-
-        window.addEventListener('load', this.resizeNavigationContent);
-        window.addEventListener('resize', this.resizeNavigationContent);
+        // Page Events
+        window.addEventListener('load', this.resizeNav)
+        window.addEventListener('resize', this.resizeNav);
     }
 
     componentWillUnmount() {
-        window.removeEventListener('resize', this.resizeNavigationContent);
-        window.removeEventListener('load', this.resizeNavigationContent);
+        // Page Events
+        window.removeEventListener('load', this.resizeNav)
+        window.removeEventListener('resize', this.resizeNav);
     }
 
     getDate = () => {
         var date = new Date();
         var weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        console.log('marker')
+
         var currWeekday = weekDays[date.getDay()];
         var currDate = months[date.getMonth()] + ' ' + date.getDate();
-        this.setState({ currentDate: [currWeekday, currDate] });
+
+        return(
+            <h1>
+                <span>{currWeekday+','}</span>
+                <span>{currDate}</span>
+            </h1>
+        );
     }
 
     submitSearch = (event) => {
@@ -108,100 +141,62 @@ export default class Navigation extends React.Component {
         searchBarRef.current.setState({ autoCompleteItems });
     }
 
-    resizeNavigationContent = () => {
-        const { selectionListRef } = this.state;
-        const selectionList = selectionListRef.current;
-        const listDimensions = selectionList.getBoundingClientRect();
-        const listItems = Object.values(selectionList.children);
-        const hiddenItems = listItems.filter(item => {
-            const itemDimensions = item.getBoundingClientRect();
-            return itemDimensions.y > listDimensions.y;
-        })
 
-        const hiddenContentItems = hiddenItems.map(item => {
-            var linkItem = item.querySelector('a');
-            return({ title: linkItem.innerHTML, link: linkItem.href });
-        })
-        this.setState({ hiddenContentItems })
-    }
+    resizeNav = () => {
+        var selectionEl = this.state.selectionListRef.current;
+        var averageWidth = 85;
+        var numVisibleItems = selectionEl.offsetWidth / averageWidth;
+        var visibleItems = this.state.selectionItems.slice(0, numVisibleItems);
+        var hiddenItems = this.state.selectionItems.slice(numVisibleItems);
 
-    getTrendingGames = async () => {
-        var gamesURL = 'https://rawg.io/api/games/lists/greatest?';
-        var searchParams = {
-            key: '065c4dbc64ad478496a7db0c70ec2765',
-            discover: true,
-            ordering: '-added',
-            page_size: 10,
-        }
-        for (const property in searchParams) {
-            gamesURL += `${property}=${searchParams[property]}&`;
-        }
-        var gameItems = await fetch(gamesURL)
-        .then(async res => res.json())
-        .then(data => data.results)
-        this.setState({ additionalSelectionItems: gameItems });
+        this.setState({ visibleItems, hiddenItems });
     }
 
     render() {
-        return(<Nav_Container>
-            <div className="navMain">
-                <Date_Logo>
-                    <StyledLogo/>
-                    {this.state.currentDate && (
-                        <h1>
-                            <span>{this.state.currentDate[0]}</span>
-                            <span>{this.state.currentDate[1]}</span>
-                        </h1>
-                    )}
-                </Date_Logo>
-                <ContentSelection>
-                    <SelectionList ref={this.state.selectionListRef}>
-                        <div><a href='https://www.ign.com/news' target='_blank'>News</a></div>
-                        <div><a href='https://www.ign.com/videos' target='_blank'>Videos</a></div>
-                        <div><a href='https://www.ign.com/reviews' target='_blank'>Reviews</a></div>
-                        <div><a href='https://www.ign.com/watch' target='_blank'>Shows</a></div>
-                        <div><a href='https://www.ign.com/wikis' target='_blank'>Wikis</a></div>
-                    </SelectionList>
-                    <SelectionList_More>
-                        <h1>More</h1>
-                        <Caret_Down/>
-                        <div>
-                            {this.state.hiddenContentItems.map((item, index) => (
-                                <a href={item.link} key={index} tabIndex='-1'>{item.title}</a>
+        return(
+            <Nav_Container>
+                <div>
+                    <Date_Logo>
+                        <StyledLogo/>
+                        {this.getDate()}
+                    </Date_Logo>
+                    <ContentSelection>
+                        <SelectionList ref={this.state.selectionListRef}>
+                            {this.state.visibleItems.map((item) => (
+                                <a href={item.link} key={item.id}>{item.title}</a>
                             ))}
-                            <a href="https://www.ign.com/wikis/ign-community-central/How_to_Follow_IGN" tabIndex='-1'>IGN on social</a>
-                            <a href="https://corp.ign.com/" tabIndex='-1'>About Us</a>
-                            <a href="https://www.ziffdavis.com/accessibility" tabIndex='-1'>Accessibility</a>
-                            <a href="https://corp.ign.com/privacy" tabIndex='-1'>Privacy Policy</a>
-                            <a href="https://corp.ign.com/user-agreement" tabIndex='-1'>Terms of Use</a>
-                            <a href="https://corp.ign.com/standards-and-practices" tabIndex='-1'>Editorial Standards</a>
-                            <a href="https://corp.ign.com/ccpa" tabIndex='-1'>Do Not Sell My Personal Information</a>
-                            <a href="https://corp.ign.com/sitemap" tabIndex='-1'>Site Map</a>
-                            <a href="https://www.ignboards.com/" tabIndex='-1'>Boards</a>
-                            <a href="https://corp.ign.com/support" tabIndex='-1'>Contact Support</a>
-                        </div>
-                    </SelectionList_More>
-                    <SearchBar
-                        ref={this.state.searchBarRef}
-                        onSubmit={this.submitSearch}
-                        searchAutoComplete={this.searchAutoComplete}
-                    />
-                    <ThemeSelection activeTheme={this.props.activeTheme} onClick={this.props.toggleTheme} tabIndex='0'>
-                        <div>
-                            {this.props.activeTheme == 'classic' ? <Sun/> : <Moon/>}
-                        </div>
-                    </ThemeSelection>
-                    <Profile>
-                        <img src={ProfileImg}/>
-                        <h1>12</h1>
-                    </Profile>
-                </ContentSelection>
-            </div>
-            <div className="navSub">
-                {this.state.additionalSelectionItems ? (this.state.additionalSelectionItems.map(item => (
-                    <a href={`https://www.google.com/search?q=${encodeURIComponent(item.name)} site:ign.com`} target='_target' key={item.id}>{item.name}</a>
-                ))) : [...Array(8)].map((item,index) => <StyledLoadingAdditionalContent key={index} />)}
-            </div>
-        </Nav_Container>);
+                        </SelectionList>
+                        <SelectionList_More>
+                            <h1>More</h1>
+                            <Caret_Down/>
+                            <div>
+                                {this.state.hiddenItems.map(item => (
+                                    <a href={item.link} key={item.id} tabIndex='-1'>{item.title}</a>
+                                ))}
+                            </div>
+                        </SelectionList_More>
+                        <SearchBar
+                            ref={this.state.searchBarRef}
+                            onSubmit={this.submitSearch}
+                            searchAutoComplete={this.searchAutoComplete}
+                        />
+                        <ThemeSelection activeTheme={this.props.activeTheme} onClick={this.props.toggleTheme} tabIndex='0'>
+                            <div>
+                                {this.props.activeTheme == 'classic' ? <Sun/> : <Moon/>}
+                            </div>
+                        </ThemeSelection>
+                        <Profile>
+                            <img src={ProfileImg}/>
+                            <h1>12</h1>
+                        </Profile>
+                    </ContentSelection>
+                </div>
+                <div>
+                    {this.state.additionalSelectionItems.map(item => (
+                        <a href={item.link} key={item.id}>{item.title}</a>
+                    ))}
+                </div>
+            </Nav_Container>
+        );
     }
 }
