cc23195922874251327b36f45e5bba9fc184c2d0
BluR3Y <69604218+BluR3Y@users.noreply.github.com>
2022-11-26 23:00:02 -0500

    Continued on development on VideoPlayer

--- ign_site/src/components/VideoPlayer.js
+++ ign_site/src/components/VideoPlayer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Controls, Header, NotificationBox, ShareVideoBtn, StyledVideoPlayer, Thumbnail } from "./styles/VideoPlayer.styled";
+import { AutoPlayBtn, Controls, Header, ShareVideoBtn, StyledVideoPlayer, Thumbnail } from "./styles/VideoPlayer.styled";
 
 export default class VideoPlayer extends React.Component {
     constructor(props) {
@@ -9,6 +9,8 @@ export default class VideoPlayer extends React.Component {
             videoInfo: this.props.videoInfo,
             activeThumbnail: null,
             activeVideo: (props.videoInfo['assets'].length ? props.videoInfo['assets'][0] : null),
+            isActive: false,
+            isPlaying: false,
             autoPlay: false,
             idleTimer: null,
             isIdle: false,
@@ -42,6 +44,46 @@ export default class VideoPlayer extends React.Component {
         this.setState({ activeThumbnail });
     }
 
+    startVideo = () => {
+        this.setState({ isActive: true });
+        this.playPauseMedia();
+    }
+
+    playPauseMedia = () => {
+        const { media } = this.state;
+
+        if(media.current.paused)
+            media.current.play();
+        else
+            media.current.pause();
+        
+        this.setState(prevState => ({ isPlaying: !prevState.isPlaying }));
+    }
+
+    toggleVolume = () => {
+
+    }
+
+    updateVolume = () => {
+
+    }
+
+    updateElapsedTime = () => {
+
+    }
+
+    setVideoProgress = () => {
+
+    }
+
+    copyToClipBoard = () => {
+
+    }
+
+    handleIdle = () => {
+
+    }
+
     HH_MM_SS = seconds => {
         return (seconds < 3600 ? 
             new Date(seconds * 1000).toISOString().substring(14, 19) :
@@ -50,25 +92,34 @@ export default class VideoPlayer extends React.Component {
     }
 
     render() {
-        const { videoInfo, activeThumbnail, activeVideo, autoPlay, idleTimer, isIdle, media, videoPlayer, volumeSlider } = this.state;
+        const { videoInfo, activeThumbnail, activeVideo, autoPlay, media, videoPlayer, volumeSlider, isActive, isPlaying } = this.state;
+        const { startVideo, playPauseMedia, toggleVolume, updateVolume, updateElapsedTime, setVideoProgress, copyToClipBoard, handleIdle} = this;
 
         return(<StyledVideoPlayer ref={videoPlayer}>
-            <Header>
-                <a>{videoInfo.metadata.title}</a>
-                <ShareVideoBtn/>
-                <NotificationBox/>
+            <Header isActive={isActive} >
+                <a href={activeVideo.url}>{videoInfo.metadata.title}</a>
+                <ShareVideoBtn onClick={copyToClipBoard} />
             </Header>
-            <Thumbnail>
-
-            </Thumbnail>
-            <video ref={media} >
+            <Thumbnail 
+                thumbnail={activeThumbnail}
+                duration={this.HH_MM_SS(videoInfo.metadata.duration)}
+                onClick={startVideo}
+                isActive={isActive}
+            />
+            <video ref={media} onClick={playPauseMedia}>
                 <source
                     src={activeVideo.url}
                     type={`video/${activeVideo.url.split('.').pop()}`}
                 />
             </video>
-            <Controls>
-                
+            <Controls isActive={isActive}>
+                <AutoPlayBtn 
+                    isPlaying={isPlaying} 
+                    isActive={isActive} 
+                    autoPlay={autoPlay} 
+                    onClick={() => this.setState(prevState => ({ autoPlay: !prevState.autoPlay }))}
+                />
+
             </Controls>
         </StyledVideoPlayer>);
     }
--- ign_site/src/components/styles/VideoPlayer.styled.js
+++ ign_site/src/components/styles/VideoPlayer.styled.js
@@ -1,6 +1,8 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import Share from '../../assets/icons/share';
+import Play from '../../assets/icons/play';
+import Play_Sharp from '../../assets/icons/play_sharp';
 
 export const StyledVideoPlayer = styled.div`
     width: 100%;
@@ -30,36 +32,33 @@ export const Header = styled.div`
     align-items: flex-start;
     justify-content: space-between;
     pointer-events: none;
-`;
 
-export const NotificationBox = styled.div`
-    position: absolute;
-    right: -100%;
-    bottom: 0;
-    border-radius: 6px;
-    background-color: #202634;
-    padding: 5px 10px;
-    
-    h1 {
+    a {
         font-family: Lato;
         font-weight: 400;
-        font-size: 18px;
-        margin: 0;
-        color: #fff;
-    }
-    @keyframes slideIn-Right {
-    0% {
-        right: -200px;
-    }10% {
-        right: 20px;
-    }90% {
-        right: 20px;
-    }100% {
-        right: -200px;
-    }
-}
-`;
+        font-size: 20px;
+        color: white;
+        text-decoration: none;
+        margin: 20px 0 0 15px;
+        pointer-events: auto;
 
+
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+    ${props => props.isActive && css`
+        background-image: linear-gradient(to bottom, black 0%, transparent 100%);
+        opacity: 0;
+        transition: opacity 0.10s linear;
+        ${StyledVideoPlayer}:hover & {
+            opacity: 1;
+        }
+    `}
+`;
 
 export const ShareVideoBtn = styled.button.attrs(() => ({
     children : (<Share/>),
@@ -74,32 +73,64 @@ export const ShareVideoBtn = styled.button.attrs(() => ({
     pointer-events: auto;
 
     svg {
-        fill: #fff;
         width: 25px;
+        fill: #fff;
     }
-
     &:hover {
         background-color: rgb(71, 71, 71);
     }
-    &:active ${NotificationBox} {
-        animation: slideIn-Right 2s linear;
-    }
 `;
 
+export const Thumbnail = styled.div.attrs((props) => ({
+    children: (<>
+            <div><Play/></div>
+            <h1>{props.duration}</h1>
+    </>)
+}))`
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    display: ${props => props.isActive ? 'none' : 'flex'};
+    align-items: center;
+    justify-content: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    z-index: 1;
+    background: ${props => props.thumbnail ? `linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) ), center / cover no-repeat url(${props.thumbnail.url})` : null};
 
+    div {
+        background-color: #bf1313;
+        border-radius: 50%;
+        width: 80px;
+        height: 80px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        pointer-events: none;
 
+        svg {
+            width: 50%;
+            margin-left:10%;
+        }
+    }
 
+    h1 {
+        position: absolute;
+        left: 20px;
+        bottom: 20px;
+        background-color: #202634;
+        padding: 5px 10px;
+        border-radius: 2px;
 
-
-
-
-
-
-
-
-
-export const Thumbnail = styled.div`
-
+        font-family: Lato;
+        font-weight: 700;
+        font-size: 18px;
+        margin: 0;
+        color: #fff;
+    }
 `;
 
 export const Controls = styled.div`
@@ -108,9 +139,101 @@ export const Controls = styled.div`
     left: 0;
     width: 100%;
     padding: 70px 0 10px 0;
-    opacity: 0;
+    visibility: hidden;
     transition: opacity 0.10s linear;
     background-image: linear-gradient(to top, black 0%, transparent 100%);
 
-
+    ${props => props.isActive && css`
+        ${StyledVideoPlayer}:hover & {
+            visibility: visible;
+        }
+    `}
 `;  
+
+export const AutoPlayBtn = styled.button.attrs((props) => ({
+    children: (<>
+        <div className='autoPlay-btn'>
+            <div className='autoPlay-thumb'>
+                <Play_Sharp/>
+            </div>
+        </div>
+        <h1>Autoplay setting: <span>{props.autoPlay ? 'On' : 'Off'}</span></h1>
+    </>)
+}))`
+    position: absolute;
+    z-index: 2;
+    height: 40px;
+    width: 220px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-radius: 8px;
+    padding: 0 10px;
+    cursor: pointer;
+    font-size: 15px;
+
+    .autoPlay-btn {
+        width: 50px;
+        height: 25px;
+        border-radius: 20px;
+        position: relative;
+
+        border: 2px solid #fff;
+    }
+    .autoPlay-thumb {
+        position: absolute;
+        border: 2px solid #fff;
+        border-radius: 50%;
+        height: inherit;
+        aspect-ratio: 1;
+        transition: left 0.10s linear;
+        display: flex;
+        align-items:center;
+        justify-content: center;
+        top: 50%;
+        transform: translateY(-50%);
+
+        svg {
+            width: 55%;
+            margin-left: 3px;
+        }
+
+        ${props => props.autoPlay ? css`
+            left: calc(50% + 2px);
+            background-color: #fff;
+            svg {
+                fill: red;
+            }
+        ` : css`
+            left: -2px;
+            svg {
+                fill: #fff;
+            }
+        `}
+    }
+
+    h1 {
+        font-size: 1em;
+        font-family: Lato;
+        font-weight: 400;
+        color: rgb(202, 201, 201);
+
+        span {
+            font-weight: 700;
+            color: #fff;
+        }
+    }
+    visibility: visible;
+    ${props => props.isActive ? css`    //controls
+        flex-direction: row;
+        top: 0;
+        left: ${props => props.isPlaying ? '-240px' : '0'};
+        transition: left 0.10s linear;
+        background-color: #202634;
+    ` : css`    // thumbnail
+        right: 20px;
+        bottom: 20px;
+        flex-direction: row-reverse;
+        background-color: transparent;
+    `}
+`;
